f425ba29e4a8a13b10b6dfd8df57f8866a06eaff
rakshit1610 <rakshitpanjwani@gmail.com>
2020-09-13 01:26:29 +0530

    before running eject command

--- src/App.js
+++ src/App.js
@@ -1,48 +1,95 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Person/Person.css';
 import Person from './Person/Person';
 
 class App extends Component {
   state={
     persons:[
-      {name:'Max', age: 28},
-      {name:'Manu', age:29},
-      {name:'Stephany',age:26}
-    ]
+      {id:'yo3' ,name:'Max', age: 28},
+      {id:'yo2', name:'Manu', age:29},
+      {id:'yo1' ,name:'Stephany',age:26}
+    ],
+    showPersons: false
   }
   
-  switchNameHandler=()=>{
-    // console.log("yo");
-    // DONT DO IT LIKE THIS:   this.state.persons[1].name='Maxmilian'
-    this.setState({
-      persons:[
-      {name:'Maxmilian', age: 28},
-      {name:'Manu', age:29},
-      {name:'Stephany',age:27}
-      ]
+
+  nameChangeHandler=(event, id)=>{
+    const personIndex= this.state.persons.findIndex(p=>{
+      return p.id === id;
     });
+
+    const person= {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name= event.target.value;
+
+    const persons=[...this.state.persons];
+    persons[personIndex]=person;
+
+    this.setState({persons:persons});
   }
 
-  nameChangeHandler=(event)=>{
-    this.setState({
-      persons:[
-        {name:'Max', age: 28},
-        {name: event.target.value, age:29},
-        {name:'Stephany',age:27}       
-      ]
-    })
+  deletePersonHandler=(personIndex)=>{
+    const persons=[...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons:persons})
   }
 
-  render(){
+  togglePersonsHandler=()=>{
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+
+  render(){ 
+    const style={
+      backgroundColor: 'green',
+      color: 'white',
+      font:'inherit',
+      border:'1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+
+
+    };
+    
+  let persons=null;
+
+  if(this.state.showPersons){
+    persons=(
+      <div>
+      {this.state.persons.map((person,index)=>{
+        return <Person click={()=>this.deletePersonHandler(index)} 
+        name={person.name} age={person.age} key={person.id} 
+        changed={(event)=>this.nameChangeHandler(event, person.id)}/>
+      })}
+
+      </div>
+    )
+
+    style.backgroundColor='red';
+  }
+
+  const classes=[];
+
+  if(this.state.persons.length<=2){
+    classes.push('red');
+  }
+
+    if(this.state.persons.length<=1){
+      classes.push('bold');
+    }
+
   return (
-    <div className="App">
-      <h1>hello world</h1>
-      <button onClick={this.switchNameHandler}>Switch Names</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
-      click={this.switchNameHandler} changed={this.nameChangeHandler}> My Hobbies: racing</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
+    <div className="App" >
+      <p className={classes.join(' ')}>hello world</p>
+      <button style={style} onClick={this.togglePersonsHandler}>Switch Names</button>
+      {persons}
     </div>
+
   );
 
   // return React.createElement('div', {className:'App'}, React.createElement('h1',null,'hello world'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+
 const person= (props)=>{
+
 return (
-<div><p onClick={props.click}>I'm {props.name}. I'm {props.age} yo</p>
+<div className="Person"><p onClick={props.click}>I'm {props.name}. I'm {props.age} yo</p>
 <p>{props.children}</p>
 <input type="text" onChange={props.changed} value={props.name}/>
 </div>
